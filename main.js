8721897b8a02154e7c23a8fc698b03f316d3417c
Antti Sykäri <antti.sykari@gmail.com>
2016-08-28 17:06:45 +0300

    Add styles, load index.html

--- main.js
+++ main.js
@@ -2,9 +2,20 @@ var { app, BrowserWindow } = require('electron');
 var window;
 
 function createWindow() {
-	window = new BrowserWindow({ width: 1280, height: 800 });
+	window = new BrowserWindow({
+		width: 1280, height: 800,
+		titleBarStyle: 'hidden'
+	});
 	window.loadURL(`file://${__dirname}/index.html`);
 }
 
 app.on('ready', createWindow);
+
+app.on('browser-window-blur', () => {
+	window.webContents.send('blur');
+});
+
+app.on('browser-window-focus', () => {
+	window.webContents.send('focus');
+});
 
