11b033a827fb6f03a5b1e0be0d354bbbc8900488
Antti Sykäri <antti.sykari@gmail.com>
2016-09-25 14:48:19 +0300

    Convert app into a redom view

--- app/app-main.js
+++ app/app-main.js
@@ -1,27 +1,47 @@
 var { $ } = require('./util');
 var { loadFile, saveFile } = require('./file');
+var { el, mount } = require('redom');
 
 var ipcRenderer = require('electron').ipcRenderer;
 
-ipcRenderer.on('blur', () => {
-	document.body.classList.add('inactive');
-});
+class App {
+	constructor() {
+		this.el = el('app.loading.toolbar-active',
+			this.titlebar = el('.titlebar', el('span.window-title')),
+			this.toolbar = el('.toolbar', el('button.button-save', 'Save')),
+			this.document = el('.document', el('textarea.content')));
+	}
 
-ipcRenderer.on('focus', () => {
-	document.body.classList.remove('inactive');
-});
+	update(file) {
+		loadFile(file);
+		app.el.classList.remove('loading');
+	}
 
-loadFile('app/app-main.js');
-document.body.classList.remove('loading');
+	blur() {
+		this.el.classList.add('inactive');
+	}
+
+	focus() {
+		this.el.classList.remove('inactive');
+	}
+}
+
+var app = new App;
+
+ipcRenderer.on('blur', () => app.blur());
+ipcRenderer.on('focus', () => app.focus());
+
+mount(document.body, app);
+app.update('app/app-main.js');
 
 $('.button-save').addEventListener('click', () => {
 	saveFile();
 });
 
-/* test hiding / showing toolbar
-$('.toolbar').addEventListener('click', function() {
-	console.log('!!! toolbar toggle');
-	document.body.classList.toggle('toolbar-active');
-});
-*/
 
+/* test hiding / showing toolbar
+ $('.toolbar').addEventListener('click', function() {
+ console.log('!!! toolbar toggle');
+ document.body.classList.toggle('toolbar-active');
+ });
+ */
